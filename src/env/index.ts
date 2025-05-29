4110177abd77552375115a784a25a5c2bceea28b
Alexandre Odoni <aleodoni@gmail.com>
2025-05-29 10:38:01 -0300

    feat: add google sign in

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,6 +10,8 @@ const envSchema = z.object({
   // Auth
   BETTER_AUTH_SECRET: z.string(),
   BETTER_AUTH_URL: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { db } from '@/db'
 
 import * as schema from '@/db/schema'
+import { env } from '@/env'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 
@@ -10,6 +11,14 @@ export const auth = betterAuth({
     usePlural: true,
     schema,
   }),
+
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+
   user: {
     modelName: 'usersTable',
   },
