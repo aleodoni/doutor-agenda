23d24d25d8f4e3055e1ed0e3b30a94e196d26b5e
Alexandre Odoni <aleodoni@gmail.com>
2025-06-02 16:41:56 -0300

    fix: clear doctor form

--- src/app/(protected)/doctors/_components/upsert-doctor-form.tsx
+++ src/app/(protected)/doctors/_components/upsert-doctor-form.tsx
@@ -145,6 +145,7 @@ export const UpsertDoctorForm = ({
   onSuccess,
 }: UpsertDoctorFormProps) => {
   const form = useForm<z.infer<typeof formSchema>>({
+    shouldUnregister: true,
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: doctor?.name ?? '',
