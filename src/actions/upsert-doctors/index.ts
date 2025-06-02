1b0d06818b3531d3df5e9249b33386c9bce58c61
Alexandre Odoni <aleodoni@gmail.com>
2025-06-02 11:00:15 -0300

    feat: save doctor aviability to UTC

--- src/actions/upsert-doctors/index.ts
+++ src/actions/upsert-doctors/index.ts
@@ -4,6 +4,8 @@ import { db } from '@/db'
 import { doctorsTable } from '@/db/schema'
 import { auth } from '@/lib/auth'
 import { actionClient } from '@/lib/next-safe-action'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
 import { headers } from 'next/headers'
 import { upsertDoctorSchema } from './schema'
 
@@ -35,9 +37,46 @@ import { upsertDoctorSchema } from './schema'
 //     })
 // }
 
+dayjs.extend(utc)
+
 export const upsertDoctor = actionClient
   .schema(upsertDoctorSchema)
   .action(async ({ parsedInput }) => {
+    const availableFromTimeMondayUTC = formatTimeToUTC(
+      parsedInput.availableFromTimeMonday
+    )
+    const availableToTimeMondayUTC = formatTimeToUTC(
+      parsedInput.availableToTimeMonday
+    )
+
+    const availableFromTimeTuesdayUTC = formatTimeToUTC(
+      parsedInput.availableFromTimeTuesday
+    )
+    const availableToTimeTuesdayUTC = formatTimeToUTC(
+      parsedInput.availableToTimeTuesday
+    )
+
+    const availableFromTimeWednesdayUTC = formatTimeToUTC(
+      parsedInput.availableFromTimeWednesday
+    )
+    const availableToTimeWednesdayUTC = formatTimeToUTC(
+      parsedInput.availableToTimeWednesday
+    )
+
+    const availableFromTimeThursdayUTC = formatTimeToUTC(
+      parsedInput.availableFromTimeThursday
+    )
+    const availableToTimeThursdayUTC = formatTimeToUTC(
+      parsedInput.availableToTimeThursday
+    )
+
+    const availableFromTimeFridayUTC = formatTimeToUTC(
+      parsedInput.availableFromTimeFriday
+    )
+    const availableToTimeFridayUTC = formatTimeToUTC(
+      parsedInput.availableToTimeFriday
+    )
+
     const session = await auth.api.getSession({
       headers: await headers(),
     })
@@ -61,9 +100,55 @@ export const upsertDoctor = actionClient
         ...parsedInput,
         clinicId,
         specialityId: parsedInput.speciality,
+        availableFromTimeMonday: availableFromTimeMondayUTC.format('HH:mm:ss'),
+        availableToTimeMonday: availableToTimeMondayUTC.format('HH:mm:ss'),
+        availableFromTimeTuesday:
+          availableFromTimeTuesdayUTC.format('HH:mm:ss'),
+        availableToTimeTuesday: availableToTimeTuesdayUTC.format('HH:mm:ss'),
+        availableFromTimeWednesday:
+          availableFromTimeWednesdayUTC.format('HH:mm:ss'),
+        availableToTimeWednesday:
+          availableToTimeWednesdayUTC.format('HH:mm:ss'),
+        availableFromTimeThursday:
+          availableFromTimeThursdayUTC.format('HH:mm:ss'),
+        availableToTimeThursday: availableToTimeThursdayUTC.format('HH:mm:ss'),
+        availableFromTimeFriday: availableFromTimeFridayUTC.format('HH:mm:ss'),
+        availableToTimeFriday: availableToTimeFridayUTC.format('HH:mm:ss'),
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
-        set: { ...parsedInput, specialityId: parsedInput.speciality },
+        set: {
+          ...parsedInput,
+          specialityId: parsedInput.speciality,
+          availableFromTimeMonday:
+            availableFromTimeMondayUTC.format('HH:mm:ss'),
+          availableToTimeMonday: availableToTimeMondayUTC.format('HH:mm:ss'),
+          availableFromTimeTuesday:
+            availableFromTimeTuesdayUTC.format('HH:mm:ss'),
+          availableToTimeTuesday: availableToTimeTuesdayUTC.format('HH:mm:ss'),
+          availableFromTimeWednesday:
+            availableFromTimeWednesdayUTC.format('HH:mm:ss'),
+          availableToTimeWednesday:
+            availableToTimeWednesdayUTC.format('HH:mm:ss'),
+          availableFromTimeThursday:
+            availableFromTimeThursdayUTC.format('HH:mm:ss'),
+          availableToTimeThursday:
+            availableToTimeThursdayUTC.format('HH:mm:ss'),
+          availableFromTimeFriday:
+            availableFromTimeFridayUTC.format('HH:mm:ss'),
+          availableToTimeFriday: availableToTimeFridayUTC.format('HH:mm:ss'),
+        },
       })
   })
+
+const formatTimeToUTC = (timeString: string) => {
+  try {
+    return dayjs()
+      .set('hour', Number.parseInt(timeString.split(':')[0]))
+      .set('minute', Number.parseInt(timeString.split(':')[1]))
+      .set('second', Number.parseInt(timeString.split(':')[2]))
+      .utc()
+  } catch (error) {
+    return dayjs().set('hour', 0).set('minute', 0).set('second', 0).utc()
+  }
+}
